48ffc24070315b9e5182cfcb5e39b5357189c3ce
Juan Aguirre <83764392+wanaguirre@users.noreply.github.com>
2022-02-03 22:26:53 +0100

    Delete repoconfing.py

--- Notebooks/repoconfing.py
+++
@@ -1,3 +0,0 @@
-local_path = "C:\Users\juana_0mrk5mk\OneDrive\Desktop\IT_stuff\Propulsion_Academy_Git (lab and hub)\Clustering-FIFA-Players"
-
-colab_path = "/content/drive/MyDrive/Colab Notebooks/GitHub/SIT Academy/Clustering-FIFA-Players"
