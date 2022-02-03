1fbd842b7cf6d1a7fac89fb52025209c0d67fe0b
Juan Aguirre <j.aguirre@urbandatalab.net>
2022-02-03 22:10:26 +0100

    setting up

---
+++ Notebooks/repoconfing.py
@@ -0,0 +1,3 @@
+local_path = "C:\Users\juana_0mrk5mk\OneDrive\Desktop\IT_stuff\Propulsion_Academy_Git (lab and hub)\Clustering-FIFA-Players"
+
+colab_path = "/content/drive/MyDrive/Colab Notebooks/GitHub/SIT Academy/Clustering-FIFA-Players"
